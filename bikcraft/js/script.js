22cf5b837d1520ca1c79d7962aa6d64e0df631be
Filipe Lima <71594975+Filipe-Design-web@users.noreply.github.com>
2021-02-21 18:02:15 -0300

    Add files via upload

---
+++ bikcraft/js/script.js
@@ -0,0 +1,18 @@
+if(window.SimpleSlide){
+    new SimpleSlide({
+        slide: 'quote', // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+    });
+
+    new SimpleSlide({
+        slide: 'port', // nome do atributo data-slide="principal"
+        nav: true,
+        time: 5000, // tempo de transição dos slides
+    });
+
+}
+
+
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
